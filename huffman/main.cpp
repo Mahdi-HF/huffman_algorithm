ca72ecebbbbc8cfd712b41814455898034ddddfc
mahdi hosseiny farzad <mhf1377@gmail.com>
2019-05-22 16:40:54 +0430

    Initial Commit

---
+++ huffman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  huffman
+//
+//  Created by mahdi on 5/22/19.
+//  Copyright © 2019 mahdi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
