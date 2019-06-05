b382b64d91aa7c94432311fbc6d11cc98f61668a
mahdi hosseiny farzad <mhf1377@gmail.com>
2019-06-05 16:48:27 +0430

    added the main design of the project

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -1,15 +1,47 @@
+//  be name khoda
 //
-//  main.cpp
-//  huffman
-//
-//  Created by mahdi on 5/22/19.
+//  huffman:
+//  Created by mahdi.hf on 5/22/19.
 //  Copyright © 2019 mahdi. All rights reserved.
-//
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+typedef long long ll;
+#define size 26
+#define fori(i,n) for(int i = 0; i < n; i++)
+
+fstream Infile;
+fstream outfile;
+
+char character;
+int count[size];
+
+int main(){
+    
+    Infile.open ("Input.txt", ios::in);
+    if (Infile.is_open())
+    {
+        while (Infile >> character)
+        {
+            if (character == ' ')
+            {
+                continue;
+            }
+            else cout << character << '\n';
+            
+        }
+        Infile.close();
+    }
+    else cout << "Unable to open file";
+//----------------------------------------------------------------------------------
+    outfile.open ("Huffman.txt", ios::out);
+    // writing ...
+    //Infile << "Writing this to a file.\n";
+    outfile.close();
+    
+    
+    cout << "Hello, World!" <<endl;
+    return 0 ;
 }
